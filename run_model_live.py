0e93a02498f4ed1443d66017c2948e092be9ba35
Dan Pomeranz <dapomeranz@Dans-MacBook-Air.local>
2023-11-16 11:42:27 -0500

    improve comparison function

--- run_model_live.py
+++ run_model_live.py
@@ -15,6 +15,9 @@ lookup = np.loadtxt("data/HT_Sensor_metadata.dat", skiprows=1, dtype=str)
 def get_class(data):
     if data["time"] < 0:
         return "background"
+    length_of_time = lookup[lookup[:, 0] == str(data["id"])][0][4]
+    if data["time"] > length_of_time:
+        return "background"
     ## lookup data["id"] in the first column of the lookup and return the 3rd column
     return lookup[lookup[:, 0] == str(data["id"])][0][2]
 
